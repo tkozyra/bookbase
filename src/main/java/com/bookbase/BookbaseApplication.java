ffdf2bbe00eeeff1791f3d633c984b206aa109ff
tkozyra <tomekkozyra@gmail.com>
2020-05-15 20:58:47 +0200

    Initial commit

---
+++ src/main/java/com/bookbase/BookbaseApplication.java
@@ -0,0 +1,13 @@
+package com.bookbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookbase/BookbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
